198afd809cbfaff89f3699f099c0609eaad152f1
Chanwut (Mick) Kittivorawong <chanwutk@gmail.com>
2022-12-05 02:52:07 -0800

    fix

--- src/app.ts
+++ src/app.ts
@@ -1,27 +1,17 @@
 import { locationIdToLocation } from './resources/locations';
-import { isDBAvailable, writeToDB } from './utils';
+import { clearRecords as clearDB, writeToDB } from './utils';
 import recordWeather from './record-weather';
 import hourlyRepeat from './hourly-repeat';
 
 const INITIAL_WEATHER_DATA = JSON.stringify(new Array(24).fill(null));
 
-// async function wait() {
-//   if (await isDBAvailable()) {
-//     start();
-//   } else {
-//     setTimeout(wait, 1000);
-//   }
-// }
-
 async function start() {
-  writeToDB('/clear-records', '');
+  clearDB();
   for (const id in locationIdToLocation) {
-    writeToDB('/modify-raw', INITIAL_WEATHER_DATA, id);
+    writeToDB(`raw${id}.json`, INITIAL_WEATHER_DATA);
   }
 
   hourlyRepeat(recordWeather)();
 }
-
-// wait();
 
 start();
