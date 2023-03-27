12e8f3b7c954d44577ca59358ed636ff2f4e3cca
Chanwut (Mick) Kittivorawong <chanwutk@gmail.com>
2023-03-27 16:05:47 -0700

    add apikeys from env

--- src/resources/api-keys-template.ts
+++
@@ -1,2 +0,0 @@
-// change this file name into api-keys.js then add your api keys here
-export default ['place-your-key-here', 'more-key-here...'];
---
+++ src/resources/api-keys.ts
@@ -0,0 +1,13 @@
+const keys: string[] = [];
+
+function getKey(i: number) {
+  const key = `PGF_KEY_${i}`;
+  return process.env[key];
+}
+
+let key: string | undefined;
+for (let i = 0; (key = getKey(i)) !== undefined; i++) {
+  keys.push(key);
+}
+
+export default keys;
