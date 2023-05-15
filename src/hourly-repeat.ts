dbcb4ef4c7504bad5a07417c9a64564eb092389f
Chanwut (Mick) Kittivorawong <chanwutk@gmail.com>
2023-05-15 14:07:48 -0700

    add email notification

--- src/hourly-repeat.ts
+++ src/hourly-repeat.ts
@@ -1,10 +1,29 @@
+import FormData from 'form-data';
+import Mailgun from 'mailgun.js';
+
 const SECOND = 1000;
 const MINUTE = SECOND * 60;
 const HOUR = MINUTE * 60;
 
 export default (callback: () => any) => {
   return async function repeat() {
-    await callback();
+    try {
+      await callback();
+    } catch (e) {
+      const mg = new Mailgun(FormData).client({
+        username: 'api',
+        key: process.env.MAILGUN_APIKEY ?? '',
+      });
+      mg.messages
+        .create(process.env.MAILGUN_DOMAIN ?? 'sandbox.mailgun.org', {
+          from: 'Pokemon Go Forecast <notification@pokemon-go-forecast>',
+          to: [process.env.MAILGUN_TO ?? 'test@gmail.com'],
+          subject: 'Pokemon Go Forecast Error Message',
+          text: `${e}`,
+        })
+        .then(msg => console.log(msg))
+        .catch(err => console.log(err));
+    }
 
     const date = new Date();
     const minute = date.getMinutes();
