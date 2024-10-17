47582ab3680f7b155a42a94c35eb53d4d94b7c3f
Chanwut (Mick) Kittivorawong <30903997+chanwutk@users.noreply.github.com>
2024-10-17 16:55:17 +0700

    Update start-worker.sh

--- start-worker.sh
+++ start-worker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 tmux new-session -d -s pokemon
-tmux send-keys -t pokemon 'cd /home/pi' Enter
+tmux send-keys -t pokemon 'cd $HOME' Enter
 tmux send-keys -t pokemon 'cd pokemon-go-forecast-worker' Enter
 tmux send-keys -t pokemon 'npm run start' Enter
 tmux attach -t pokemon
